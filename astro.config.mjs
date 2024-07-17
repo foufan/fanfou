552adcdacd266019cb3eb441907d72ccb7fed300
openwj <openwj@hotmail.com>
2024-07-17 16:34:14 +0800

    fit:初始化博客项目文件

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "尚能饭否?",
+      locales: {
+				root: {
+					label: '简体中文',
+					lang: 'zh-CN',
+				},
+			},
+      social: {
+				// github: 'https://github.com/foufan',
+			},
+    }),
+  ],
+});
