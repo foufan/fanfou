cf0f9624f7418bde51c5eb445f47719e6cfd6fa6
openwj <openwj@hotmail.com>
2024-07-18 12:42:31 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,32 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fou.fan',
-  base: '/',
+  site: "https://fou.fan",
+  base: "/",
   integrations: [
     starlight({
       title: "饭否",
       locales: {
-				root: {
-					label: '简体中文',
-					lang: 'zh-CN',
-				},
-			},
+        root: {
+          label: "简体中文",
+          lang: "zh-CN",
+        },
+      },
       social: {
-				// github: 'https://github.com/foufan',
-			},
+        // github: 'https://github.com/foufan',
+      },
+      sidebar: [
+        {
+          label: "前端笔记",
+          // 自动生成一个链接分组
+          autogenerate: { directory: "frontend" },
+        },
+        {
+          label: "后端笔记",
+          // 自动生成一个链接分组
+          autogenerate: { directory: "backend" },
+        },
+      ],
     }),
   ],
 });
