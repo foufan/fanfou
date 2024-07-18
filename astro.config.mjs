4f8c1202dcaec045881f6a63735a10ce0c3904b9
openwj <138089328+openwj@users.noreply.github.com>
2024-07-18 10:33:22 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://foufan.github.io',
+  base: '/foufan.github.io/',
   integrations: [
     starlight({
       title: "尚能饭否?",
