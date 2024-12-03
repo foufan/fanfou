57379ca55a152b166733a9148e5d49e8dd994e82
openwj <openwj@hotmail.com>
2024-12-03 22:58:55 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://fou.fan',
 	integrations: [mdx(), sitemap(), tailwind()],
 });
