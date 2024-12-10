a81c56740428a370f0e13651de3a85759eae2946
openwj <openwj@hotmail.com>
2024-12-10 11:48:50 +0800

    update

--- src/utils/getCoverImage.ts
+++ src/utils/getCoverImage.ts
@@ -6,7 +6,7 @@ export function getCoverImage(customCoverPath?: string): string {
     }
   
     // 如果没有提供自定义封面图片路径，则从 public 文件夹中随机选择一张图片
-    const randomImageIndex = Math.floor(Math.random() * 7) + 1;  // 生成 1 到 7 的随机整数
+    const randomImageIndex = Math.floor(Math.random() * 18) + 1;  // 生成 1 到 18 的随机整数
     return `/blog-${randomImageIndex}.webp`;  // 返回路径，如：/blog-1.webp 到 /blog-7.webp
   }
   
